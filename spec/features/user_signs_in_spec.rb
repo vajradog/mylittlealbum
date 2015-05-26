83b5dd1cc0a5a6bf8e5e80e10442e928c62bad0c
vajradog <vajradog@gmail.com>
2015-05-25 23:24:07 -0400

    refactor 2

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature "user signs in with valid data" do
+feature "user signs in" do
   scenario "with valid credentials and user is signed in" do
     thupten = Fabricate(:user)
     sign_in(thupten)
@@ -12,29 +12,26 @@ feature "user signs in with valid data" do
     sign_in(thupten)
     expect_current_path_to_be(photos_path)
   end
-end
 
-feature "user signs in with invalid data" do 
   scenario "with invalid credentials and is not redirected to photos_path" do
     sign_in_with_invalid_credential
     expect_current_path_to_be(root_path)
   end
-end
 
-def expect_page_to_have_full_name(user)
-  expect(page).to have_content(user.full_name)
-end
+  def expect_page_to_have_full_name(user)
+    expect(page).to have_content(user.full_name)
+  end
 
-def expect_current_path_to_be(url)
-  expect(current_path).to eq(url)
-end
+  def expect_current_path_to_be(url)
+    expect(current_path).to eq(url)
+  end
 
-
-def sign_in_with_invalid_credential
-  user = Fabricate(:user)
-  visit root_path
-  click_on('Sign in')
-  fill_in('user[full_name]', with: user.full_name)
-  fill_in('user[email]', with: user.email)
-  fill_in('user[password]', with: "wrong_password")  
+  def sign_in_with_invalid_credential
+    user = Fabricate(:user)
+    visit root_path
+    click_on('Sign in')
+    fill_in('user[full_name]', with: user.full_name)
+    fill_in('user[email]', with: user.email)
+    fill_in('user[password]', with: "wrong_password")  
+  end
 end
